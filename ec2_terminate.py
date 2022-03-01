cdde44f5f4f0bb2f35959e780366aabcd9023cc7
John <34498731+yogabull@users.noreply.github.com>
2022-02-28 23:13:22 -0600

    Update ec2_terminate.py

--- ec2_terminate.py
+++ ec2_terminate.py
@@ -1,5 +1,6 @@
 import boto3
 
+# Add regions to check.
 aws_regions = [
     'us-east-1',
     'us-east-2',
@@ -7,26 +8,20 @@ aws_regions = [
     'us-west-2',]
  
 def lambda_handler(event, context):
-    for region in aws_regions: # Get instances in each region.
+    for region in aws_regions: # Loop through ec2_clients in each region.
         ec2_client = boto3.client('ec2', region_name=region)
         response = ec2_client.describe_instances()
-        ec2_instances = response['Reservations']
+        
+        ec2_instances = response['Reservations'] # Get a list of all instances.
         
         ec2_list =[]
         for instance in ec2_instances: # Get EC2 id list.
             ec2_list.append(instance['Instances'][0]['InstanceId'])
-           
-        print(f"{region}: ", ec2_list)
-        
-        
-        
-        
-        
-#       for ec2 in ec2_list:
-#           print(f"Terminating:", ec2)
-#           terminate_instance(ec2, region)
 
-# def terminate_instance(instance_id, region):
-#     ec2_client = boto3.client("ec2", region_name="region")
-#     response = ec2_client.terminate_instances(InstanceIds=instance_id)
-#     print(response)
+        for ec2 in ec2_list:
+            print(f"{region} Terminating: ", ec2)
+            terminate_instance(ec2, region, ec2_client)
+
+def terminate_instance(instance_id, region, ec2_client):
+    response = ec2_client.terminate_instances(InstanceIds=[instance_id])
+    
