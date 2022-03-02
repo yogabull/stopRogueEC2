830937ff66750c7833e4fb5d9781b3890d2964b4
John <34498731+yogabull@users.noreply.github.com>
2022-03-01 21:43:58 -0600

    Changed 'terminate. to 'stop'.

--- ec2_terminate.py
+++ ec2_terminate.py
@@ -6,7 +6,7 @@ aws_regions = [
     'us-east-2',
     'us-west-1',
     'us-west-2',]
- 
+    
 def lambda_handler(event, context):
     for region in aws_regions: # Loop through ec2_clients in each region.
         ec2_client = boto3.client('ec2', region_name=region)
@@ -19,9 +19,8 @@ def lambda_handler(event, context):
             ec2_list.append(instance['Instances'][0]['InstanceId'])
 
         for ec2 in ec2_list:
-            print(f"{region} Terminating: ", ec2)
-            terminate_instance(ec2, region, ec2_client)
+            print(f"{region} Stopping: ", ec2)
+            stop_instance(ec2, region, ec2_client)
 
-def terminate_instance(instance_id, region, ec2_client):
-    response = ec2_client.terminate_instances(InstanceIds=[instance_id])
-    
+def stop_instance(instance_id, region, ec2_client):
+    response = ec2_client.stop_instances(InstanceIds=[instance_id])
