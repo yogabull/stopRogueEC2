99a4f96c4fcadac579d2726bbe5560c7c79e8c24
yogabull <yogabull@gmail.com>
2022-04-03 23:28:59 -0500

    Added loop to create a region list.

--- ec2_stop.py
+++ ec2_stop.py
@@ -1,23 +1,26 @@
 import boto3
 
-# Add regions to check.
-aws_regions = [
-    'us-east-1',
-    'us-east-2',
-    'us-west-1',
-    'us-west-2',]
-    
 def lambda_handler(event, context):
-    for region in aws_regions: # Loop through ec2_clients in each region.
+    ec2_client = boto3.client('ec2')
+    
+    # Add regions to check.
+    regions = ec2_client.describe_regions()['Regions']
+    aws_regions = []
+    for region in regions:
+        aws_regions.append(region['RegionName'])
+
+    # Loop through ec2_clients in each region.
+    for region in aws_regions: 
         ec2_client = boto3.client('ec2', region_name=region)
         response = ec2_client.describe_instances()
         
-        ec2_instances = response['Reservations'] # Get a list of all instances.
-        
+        # Get a EC2 id lists.
+        ec2_instances = response['Reservations'] 
         ec2_list =[]
-        for instance in ec2_instances: # Get EC2 id list.
+        for instance in ec2_instances:
             ec2_list.append(instance['Instances'][0]['InstanceId'])
-
+            
+        # Stop instances in list.
         for ec2 in ec2_list:
             print(f"{region} Stopping: ", ec2)
             stop_instance(ec2, region, ec2_client)
